ac8d6c49d95b1b25854d055862bae50bbfffeaff
anggreaniliolita <Anggreani3110@gmail.com>
2022-01-25 09:48:07 +0700

    layout Anggreani

--- app/src/main/java/ac/id/atmaluhur/mhs/WISATAAPPS/About.java
+++ app/src/main/java/ac/id/atmaluhur/mhs/WISATAAPPS/About.java
@@ -2,13 +2,23 @@ package ac.id.atmaluhur.mhs.WISATAAPPS;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class About extends AppCompatActivity {
-
+Button kembali;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        kembali = (Button)findViewById(R.id.button);
+        kembali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(About.this, MenuUtama2.class));
+            }
+        });
     }
 }
